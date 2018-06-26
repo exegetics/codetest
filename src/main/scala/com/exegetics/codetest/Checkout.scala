f39b4e82c475dead29bcf33056094d0d3e715355
ben <ben.stapley@exegetics.co.uk>
2018-06-26 18:12:48 +0100

    Completed step two: added a couple of comments

--- src/main/scala/com/exegetics/codetest/Checkout.scala
+++ src/main/scala/com/exegetics/codetest/Checkout.scala
@@ -5,16 +5,19 @@ import com.exegetics.codetest.fruit.{Apple, Fruit, Orange}
 
 object Checkout {
 
-  // Offers
+  // Offers (calculates discount on basket)
+  //buy one get one free on apples
   val bogofApples = (basket: Basket) => {
     // get all the apples; pair them up and discount the second apple (should deal with odd numbers of apples)
     basket.items.collect { case x: Apple => x }.sliding(2, 2).collect { case Seq(a, b) => b.price }.sum
   }
 
+  // three for two on oranges
   val threeForTwoOranges = (basket: Basket) => {
-    // modified bogofApples (
+    // modified bogofApples
     basket.items.collect { case x: Orange => x }.sliding(3, 3).collect { case Seq(a, b, c) => c.price }.sum
   }
+
   //entry point
   def main(args: Array[String]): Unit = {
     val basket = Basket(args)
